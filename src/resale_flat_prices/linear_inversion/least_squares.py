bd56e42386f94caaf50a955e2c541b1910f78ab7
user <user@192.168.1.9>
2025-01-14 17:50:41 +0800

    Updated linear inversion functions. Added minimum length solution functions.

--- src/resale_flat_prices/linear_inversion/least_squares.py
+++ src/resale_flat_prices/linear_inversion/least_squares.py
@@ -13,3 +13,17 @@ def least_squares(G, d):
     m = np.dot(np.linalg.inv(np.dot(G.T, G)), G.T)
     m = np.dot(m, d)
     return m
+
+
+def minimum_length(G, d):
+    """
+    Minimum length solution to the underdetermined least squares problem.
+    Inputs
+        G: array
+        d: array
+    Outputs
+        m: array
+    """
+    m = np.dot(G.T, np.linalg.inv(np.dot(G, G.T)))
+    m = np.dot(m, d)
+    return m
--- src/resale_flat_prices/linear_inversion/linear_inversion.py
+++ src/resale_flat_prices/linear_inversion/linear_inversion.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 # Local imports.
-from resale_flat_prices.linear_inversion.least_squares import least_squares
+from resale_flat_prices.linear_inversion.least_squares import least_squares, minimum_length
 from resale_flat_prices.linear_inversion.l1_norm_inversion import l1_norm_inversion
-from resale_flat_prices.linear_inversion.linear_inversion_metrics import r2
 
 
 class LinearInversion:
@@ -23,13 +22,17 @@ class LinearInversion:
     def fit(self, X, y, sd = None, vander_order = None):
         assert self.model is not None
 
-        G = self.make_data_kernel(X, vander_order)
         self.m = None
-        if self.error_type == "l2":
-            self.m = self.model(G, y)
-        elif self.error_type == "l1":
-            self.m = self.model(G, y, sd)
+        G = self.make_data_kernel(X, vander_order)
         
+        try:
+            if self.error_type == "l2":
+                self.m = self.model(G, y)
+            elif self.error_type == "l1":
+                self.m = self.model(G, y, sd)
+        except np.linalg.LinAlgError:
+            self.m = minimum_length(G, y)
+
         return self.m
 
 
@@ -43,4 +46,9 @@ class LinearInversion:
     def make_data_kernel(self, X, vander_order = None):
         if vander_order is None:
             vander_order = self.vander_order
+
+        if len(X.shape) > 1:
+            if X.shape[1] == 1:
+                X = X.reshape(-1)
+
         return np.vander(X, vander_order)
